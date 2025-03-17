7115e1cb1c228a26680e4114aa692eeeb620a6f4
Elkim Medranda <elkinmedranda47@example.com>
2025-03-17 01:17:20 -0500

    se protegen los perfiles profesor y estudiantes,se agrega el guard a los componetnes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './custom/auth.guard';
+import { EstudianteComponent } from './components/estudiante/estudiante.component';
+import { ProfesorComponent } from './components/profesor/profesor.component';
 
 export const routes: Routes = [
 
@@ -9,6 +11,10 @@ export const routes: Routes = [
     { path: '',redirectTo:'login', pathMatch:'full'} 
     ,
     { path: 'login',component:LoginComponent}
+    ,
+    { path: 'estudiante',component:EstudianteComponent,canActivate: [authGuard] }
+    ,
+    { path: 'login',component:ProfesorComponent,canActivate: [authGuard] }
     ,
     //carga peresosa, se cargara el modulo solo cunado la ruta sea {path: 'dashboard'}
     { path: 'dashboard',loadChildren:() => import('./components/dashboard/dashboard.module').then(x=>x.DashboardModule)}
